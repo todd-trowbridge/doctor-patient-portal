7f6839256a7677c527ba92bb502bbc51296f2fe9
Todd Trowbridge <todd.m.trowbridge@gmail.com>
2022-04-22 11:15:10 -0400

    added in-memory spring security authentication

---
+++ backend/src/main/java/com/todd/trowbridge/ibex/interview/backend/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.todd.trowbridge.ibex.interview.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.cert.Extension;
+
+
+@Configuration // Spring Security Configuration
+@EnableWebSecurity // Used to enable web security and select urls for protection
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // create admin in memory
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("1234")).authorities("USER","ADMIN");
+    }
+
+    
+    @Bean
+    // create password encoder using BCrypt
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // permit any request that comes to this application
+//        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
+
+        http.formLogin();
+    }
+}
