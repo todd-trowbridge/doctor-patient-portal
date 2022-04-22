a41efad711aef12bc1c4c2f68bac0cbe708890f5
Todd Trowbridge <todd.m.trowbridge@gmail.com>
2022-04-22 10:23:37 -0400

    Doctor Patient Portal Demo Project

---
+++ backend/src/test/java/com/todd/trowbridge/ibex/interview/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todd.trowbridge.ibex.interview.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
